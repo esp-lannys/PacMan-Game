b7116c6b4a1d8a8b2a1b68d51ec19571f3dc5eac
nphoangtu <45940412+nphoangtu@users.noreply.github.com>
2019-05-31 14:24:36 +0700

    Update Main.java

--- main/Main.java
+++ main/Main.java
@@ -21,24 +21,7 @@ public class Main {
          frame.setVisible(true);
          view.requestFocus();
          view.start();
-       /* SwingUtilities.invokeLater(new Runnable() {
-
-            @Override
-            public void run() {
-                Game game = new PacmanGame();
-                Display view = new Display(game);
-                JFrame frame = new JFrame();
-                frame.setTitle("Pacman");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.getContentPane().add(view);
-                frame.pack();
-                frame.setLocationRelativeTo(null);
-                frame.setVisible(true);
-                view.requestFocus();
-                view.start();
-            }
-
-        });*/
+       
     }
     
 }
