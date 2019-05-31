83b03adf5578da55465d31b3e354c84efa8b6fef
nphoangtu <45940412+nphoangtu@users.noreply.github.com>
2019-05-31 15:23:29 +0700

    draw rendered font and handle Actor

--- back/Game.java
+++ back/Game.java
@@ -7,11 +7,6 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Game class.
- * 
- * @author Leonardo Ono (ono.leo@gmail.com)
- */
 public class Game {
     
     public Dimension screenSize;
@@ -33,21 +28,6 @@ public class Game {
         for (Actor actor : actors) {
             actor.draw(g);
         }
-    }
-
-    public <T> T checkCollision(Actor a1, Class<T> type) {
-        a1.updateCollider();
-        for(Actor a2 : actors) {
-            a2.updateCollider();
-            if (a1 != a2 
-                && type.isInstance(a2)
-                && a1.collider != null && a2.collider != null
-                && a1.visible && a2.visible
-                && a2.collider.intersects(a1.collider)) {
-                    return type.cast(a2);
-            }
-        }
-        return null;
     }
 
     public void broadcastMessage(String message) {
