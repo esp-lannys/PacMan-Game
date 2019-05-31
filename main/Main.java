a8d0189a7374bc0fbc7520d947a96ee848dd0468
nphoangtu <45940412+nphoangtu@users.noreply.github.com>
2019-05-31 13:41:58 +0700

    Main

---
+++ main/Main.java
@@ -0,0 +1,44 @@
+package main;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+import back.Display;
+import back.Game;
+import pacman.PacmanGame;
+
+public class Main {
+
+    public static void main(String[] args) {
+    	 Game game = new PacmanGame();
+         Display view = new Display(game);
+         JFrame frame = new JFrame();
+         frame.setTitle("Pacman");
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         frame.getContentPane().add(view);
+         frame.pack();
+         frame.setLocationRelativeTo(null);
+         frame.setVisible(true);
+         view.requestFocus();
+         view.start();
+       /* SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                Game game = new PacmanGame();
+                Display view = new Display(game);
+                JFrame frame = new JFrame();
+                frame.setTitle("Pacman");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.getContentPane().add(view);
+                frame.pack();
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+                view.requestFocus();
+                view.start();
+            }
+
+        });*/
+    }
+    
+}
